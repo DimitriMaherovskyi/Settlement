75ef6ece2dabbeb59bfcad09354c73b7979b506e
Dmytro Maherovskyi <Dmytro Maherovskyi@MDV>
2016-06-23 21:19:37 +0300

    HostelController added

---
+++ Settlement/modules/eQuiz.Web/Controllers/HostelRoomsController.cs
@@ -0,0 +1,108 @@
+﻿using Settlement.Repositories.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Settlement.Entities;
+using Settlement.Web.Models;
+
+namespace Settlement.Web.Controllers
+{
+    public class HostelRoomsController : Controller
+    {
+        #region Fields
+
+        private readonly IRepository _repository;
+
+        #endregion
+
+        #region Constructors
+
+        public HostelRoomsController(IRepository repository)
+        {
+            this._repository = repository;
+        }
+
+        #endregion
+
+        #region Web actions
+
+        [HttpGet]
+        public JsonResult GetHostelInfo(int id)
+        {
+            var h = _repository.GetSingle<Entities.Hostel>(hh => hh.Id == id);
+
+            var hostel = new Models.Hostel(h.Id, h.Number, h.Address, h.MonthPaymentSum);
+
+            return Json(hostel, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult GetHostelRooms(int hostelId)
+        {
+            var setteledStudents = new List<StudentsReview>();
+            var setteledRooms = new List<object>();
+
+            var students = _repository.Get<Student>();
+
+            var studentRooms = _repository.Get<StudentRoom>();
+            var rooms = _repository.Get<Entities.Room>();
+            var hostels = _repository.Get<Entities.Hostel>();
+
+            var studentBenefits = _repository.Get<StudentBenefit>();
+            var benefits = _repository.Get<Benefit>();
+
+            var studentViolations = _repository.Get<StudentViolation>();
+
+            var setteled = from s in students
+                           join sr in studentRooms on s.Id equals sr.StudentId
+                           join r in rooms on sr.RoomId equals r.Id
+                           join h in hostels on r.HostelId equals h.Id
+                           where r.HostelId == hostelId
+                           select new StudentsReview(s.Id, s.Firstname + " " + s.Surname, h.Number, r.Number, s.Insitute, false);
+
+            foreach (var item in setteled)
+            {
+                setteledStudents.Add(item);
+            }
+
+            var query = from r in rooms
+                        join sr in studentRooms on r.Id equals sr.RoomId
+                        join s in setteledStudents on sr.StudentId equals s.Id
+                        group new { r, s } by r.Id into grouped
+                        select new RoomStuds(
+                            grouped.Key,
+                            grouped.Select(g => g.r.Number).Single(),
+                            grouped.Select(g => g.r.AmountPlaces).Single(),
+                            grouped.Select(g => g.r.RoomFloor).Single(),
+                            grouped.Select(g => g.r.HostelId).Single(),
+                            grouped.Select(g => g.s).ToList(),
+                            false
+                            );
+
+            foreach (var item in query)
+            {
+                setteledRooms.Add(item);
+            }
+
+            return Json(setteledRooms, JsonRequestBehavior.AllowGet);
+        }
+
+        #endregion
+
+        #region Post methods
+
+        public void UpdateHostelInfo(int id, int number, string address, int monthPayment)
+        {
+            var h = _repository.GetSingle<Entities.Hostel>(hh => hh.Id == id);
+
+            h.Number = number;
+            h.Address = address;
+            h.MonthPaymentSum = monthPayment;
+
+            _repository.Update<Entities.Hostel>(h);
+        }
+
+        #endregion
+    }
+}
---
+++ Settlement/modules/eQuiz.Web/Models/RoomStuds.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Settlement.Web.Models;
+
+namespace Settlement.Web.Models
+{
+    public class RoomStuds
+    {
+        private List<StudentsReview> students;
+
+        public int Id { get; set; }
+        public int Number { get; set; }
+        public int AmountPlaces { get; set; }
+        public int RoomFloor { get; set; }
+        public int HostelId { get; set; }
+        public List<StudentsReview> Students
+        {
+            get
+            {
+                return students;
+            }
+
+            set
+            {
+                students = value;
+            }
+        }
+
+        public bool HasRoomProblems { get; set; }
+
+
+        public RoomStuds(int id, int number, int amount, int floor, int hostelId, List<StudentsReview> students, bool hasProblems)
+        {
+            Id = id;
+            Number = number;
+            AmountPlaces = amount;
+            RoomFloor = floor;
+            HostelId = hostelId;
+            Students = students;
+            HasRoomProblems = hasProblems;
+        }
+
+    }
+}
